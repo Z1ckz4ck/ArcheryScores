22114559ae1b4efe2e86c981462e92585114b294
z1ckz4ck <rscheffel00@gmail.com>
2017-07-22 20:58:54 +0200

    Adapter umgebaut

---
+++ app/src/main/java/eu/scp/z1ckz4ck/archeryscores/activity/SelectParcour.java
@@ -0,0 +1,16 @@
+package eu.scp.z1ckz4ck.archeryscores.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import eu.scp.z1ckz4ck.archeryscores.entity.Parcour;
+
+import java.util.List;
+
+public class SelectParcour extends AppCompatActivity {
+
+    private static final String TAG = "SelectPacour";
+
+    List<Parcour> pacours;
+    //TODO:...
+
+
+}
